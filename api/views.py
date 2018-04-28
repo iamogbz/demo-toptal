1906bfff8dd40509a3e65edf6049862b84e847b8
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-04-28 16:12:50 -0400

    show list of account managers

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from rest_framework import viewsets
 
 from api import models
 from api import serializers
+from api import constants
 
 
 class ScopeViewSet(viewsets.ModelViewSet):
@@ -54,6 +55,28 @@ class AccountViewSet(viewsets.ModelViewSet):
             self.perform_update(serializer)
             response = Response(serializer.data)
 
+        return response
+
+    @list_route(methods=['GET', 'POST'])
+    def managers(self, request, *_, **__):
+        """
+        Handle listing and adding accounts that can manage user
+        """
+        mgr_scope = models.Scope.objects.get(
+            codename=constants.PermissionCodes.Account.MANAGE,
+        )
+        if request.method == 'GET':
+            mgrs = [auth.owner for auth in models.Auth.objects.filter(
+                user=request.user,
+                active=True,
+            ) if mgr_scope.id in auth.granted]
+            serializer = self.get_serializer(
+                models.Account.objects.filter(pk__in=mgrs),
+                many=True,
+            )
+            response = Response(serializer.data)
+        elif request.method == 'POST':
+            pass
         return response
 
 
