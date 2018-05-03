91ba81fa4cdd9fc5beaa6f1c43d26b8e39c85007
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2018-05-02 23:46:07 -0400

    refactor jogger settings test

--- jogger/tests.py
+++ jogger/tests.py
@@ -14,7 +14,18 @@ class SettingsTest(TestCase):
         """
         Test dot env loaded enviroment settings
         """
-        self.assertIsNotNone(os.getenv('SECRET'))
-        self.assertIsNotNone(os.getenv('DEBUG'))
-        self.assertIsNotNone(os.getenv('DB_NAME'))
-        self.assertIsNotNone(os.getenv('DB_TEST'))
+        secret_key = os.getenv('SECRET')
+        self.assertIsNotNone(secret_key)
+        self.assertGreater(len(secret_key), 32)
+
+        debug_flag = os.getenv('DEBUG')
+        self.assertIsNotNone(debug_flag)
+        self.assertEqual(len(debug_flag), 1)
+
+        db_name = os.getenv('DB_NAME')
+        self.assertIsNotNone(db_name)
+        self.assertGreater(len(db_name), 0)
+
+        db_test = os.getenv('DB_TEST')
+        self.assertIsNotNone(db_test)
+        self.assertGreater(len(db_test), 0)
